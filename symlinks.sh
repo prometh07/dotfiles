ae7f591e7abff7fab8c7aae999cbecebe210b33a
Radosław Luter <radekpl2@gmail.com>
2014-11-08 17:40:45 +0100

    terminator solarized theme added

--- symlinks.sh
+++ symlinks.sh
@@ -9,6 +9,7 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="vimrc vim gitconfig gitignore"    # list of files/folders to symlink in homedir
+terminator_dir="$HOME/.config/terminator"
 
 ##########
 
@@ -24,8 +25,17 @@ echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+  echo "Moving any existing dotfiles from ~ to $olddir"
+  mv ~/.$file ~/dotfiles_old/
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/.$file
 done
+
+# add terminator dotfiles
+if [ -d $HOME/.config/terminator ]
+then
+  echo "Moving the old terminator directory to $olddir"
+  mv $HOME/.config/terminator $olddir/
+fi
+echo "Creating symlink to terminator in $terminator_dir."
+ln -s $dir/terminator/ $terminator_dir
