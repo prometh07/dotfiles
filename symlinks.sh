ebc0caadedc7320fad1b91b20b426c46a2851ae5
nvc <radekpl2@gmail.com>
2017-05-12 11:51:04 +0200

    refactor

--- symlinks.sh
+++ symlinks.sh
@@ -1,42 +1,31 @@
 #!/bin/bash
+dotfiles=$HOME/dotfiles
+date=`date +"%Y-%m-%d_%H-%M"`
+dotfiles_old=$HOME/dotfiles_old/$date
+mkdir -p $dotfiles_old/$date
 
-########## Variables
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim gitconfig gitignore atom"    # list of files/folders to symlink in homedir
-terminator_dir="$HOME/.config/terminator"
+files=".vimrc .vim .gitconfig .gitignore"
+dirs=".vim .config/terminator .atom"
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+echo "Creating backup in $dotfiles_old"
+for file in $files; do mv $HOME/$file $dotfiles_old; done
+for dir in $dirs; do mv $HOME/$dir $dotfiles_old; done
 
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-for file in $files; do
-  echo "Moving any existing dotfiles from ~ to $olddir"
-  mv ~/.$file ~/dotfiles_old/
-  echo "Creating symlink to $file in home directory."
-  ln -s $dir/$file ~/.$file
+echo "Copying new dotfiles from $dotfiles"
+cd $dotfiles
+for file in $files; do ln -s $dotfiles/$file $HOME/$file; done
+for dir in $dirs; do
+  if [[ "$dir" == *"/"* ]]
+  then
+    ln -s $dotfiles/$dir $HOME/${dir%/*}
+  else
+    ln -s $dotfiles/$dir $HOME
+  fi
 done
 
-# add terminator dotfiles
-if [ -d $HOME/.config/terminator ]
-then
-  echo "Moving the old terminator directory to $olddir"
-  mv $HOME/.config/terminator $olddir/
-fi
-echo "Creating symlink to terminator in $terminator_dir."
-ln -s $dir/terminator/ $terminator_dir
-
-# install atom packages
-if [ ! -d ~/.atom/packages/package-sync ]; then
-  if type apm > /dev/null 2>&1; then
-    echo "Installing Atom package-sync package."
-    apm install package-sync
-  fi
-fi
+#if [ ! -d ~/.atom/packages/package-sync ]; then
+#  if type apm > /dev/null 2>&1; then
+#    echo "Installing Atom package-sync package."
+#    apm install package-sync
+#  fi
+#fi
